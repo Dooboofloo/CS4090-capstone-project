95171aaf4f8c74e8f32ff5309bb45502a2a82f5c
WeAreRico <164869558+WeAreRico@users.noreply.github.com>
2024-12-02 21:43:11 -0600

    removed unnecessary comments

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -3,13 +3,10 @@ extends VBoxContainer
 const MAIN_SCENE = preload("res://indev/Caveman Age Map Stuff/Caveman Scene.tscn")
 const ROTATION_SPEED = 0.2
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$"../VBoxContainer2/Score".text = "Score: %s" % Global.PLAYER_SCORE
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# lol putting this in _process is kinda wack but fuck it # <- Real shit
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 func _on_new_game_button_pressed() -> void:
